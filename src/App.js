ceceb518dd3d4e1d3d81b32805642beeb0e3e1ee
Priya1514-coder <85052217+Priya1514-coder@users.noreply.github.com>
2021-06-07 14:27:03 +0530

    This is a Counter Basic Web App in React.js Using Hooks Consept

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
+import React, { useState } from "react";
 import "./styles.css";
+//import {useState} from 'react';
 
-export default function App() {
+//let count =1;
+
+const App = () => {
+  const [count, setCount] = useState(0);
+
+  const IncrNum = () => {
+    setCount(count + 1);
+    //console.log("Increment");
+  };
+
   return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+    <div>
+      <h1>Counter App</h1>
+      <h2>{count}</h2>
+      <button onClick={IncrNum}> Click Me</button>
     </div>
   );
-}
+};
+export default App;
